b88ab02a6aed9385aacaf9d89f621731cd19f144
ArianVangle <ariansemenoff@yandex.ru>
2026-03-23 15:28:24 +0300

    formatting

--- lab_1/interface.h
+++ lab_1/interface.h
@@ -4,59 +4,38 @@
 #include "algebra.h"
 #include "matrix.h"
 
-// Запуск интерактивного режима
 void run_interactive_mode(void);
-
-// Отображение главного меню
 void show_main_menu(void);
 
-// Подменю для Integer матриц
 void show_integer_menu(void);
 void integer_menu(void);
 
-// Подменю для Complex матриц
 void show_complex_menu(void);
 void complex_menu(void);
 
-// Ввод матрицы Integer с клавиатуры
 Matrix* input_integer_matrix(int size);
-
-// Ввод матрицы Complex с клавиатуры
 Matrix* input_complex_matrix(int size);
-
 Matrix* input_double_matrix(int size);
-
-// Ввод скаляра Integer
 Integer input_integer_scalar(void);
-
-// Ввод скаляра Complex
 Complex input_complex_scalar(void);
-// Ввод вектора Double с клавиатуры
 Matrix* input_double_vector(int size);
 
-// integer matrix
 void integer_manual_create(void);
 void integer_add_manual(void);
 void integer_multiply_manual(void);
 void integer_scalar_manual(void);
 void integer_demo(void);
 
-// complex matrix
 void complex_manual_create(void);
 void complex_add_manual(void);
 void complex_multiply_manual(void);
 void complex_scalar_manual(void);
 void complex_demo(void);
 
-// LU
 void lu_decomposition_demo(void);
-// Демо типобезопасности
 void demo_type_safety(void);
-
-// Очистка буфера ввода
 void clear_input_buffer(void);
 
-// QR
 void qr_decomposition_demo(void);
 void solve_slau_demo(void);
 void benchmark_demo(void);
