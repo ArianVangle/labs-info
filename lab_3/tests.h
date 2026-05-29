cc8b9d2dc1a14bbaed5180cd51721d6fbcf2ce50
ArianVangle <ariansemenoff@yandex.ru>
2026-05-29 15:29:57 +0300

    update hanoi IsComplete and update tests with new structure

--- lab_3/tests.h
+++ lab_3/tests.h
@@ -2,12 +2,20 @@
 #include "from_lab_2/tests.h"
 
 class Lab3TestRunner : public TestRunner {
-   private:
-    void TestDeque();
-    void TestHanoiAndRing();
+private:
+    void TestDequeBasicOps();
+    void TestDequePopClearAndEdge();
+    void TestDequeMapReduceIterators();
+    void TestDequeSortConcatSubsequence();
+    void TestDequeExceptions();
 
-   public:
-    Lab3TestRunner(ConsoleUI *ui) : TestRunner(ui) {}
+    void TestHanoiConstruction();
+    void TestHanoiSolvingLogic();
+    void TestHanoiHistoryAndStats();
+    void TestHanoiExceptions();
+    void TestRingOperations();
 
+public:
+    Lab3TestRunner(ConsoleUI* ui = nullptr) : TestRunner(ui) {}
     void RunAll();
 };
